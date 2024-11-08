751b97e90f91075b344f9a6649109dbbc6d4497d
MadsKrist <163913343+MadsKrist@users.noreply.github.com>
2024-11-08 12:36:56 +0100

    Update sound.js

--- src/lib/sound.js
+++ src/lib/sound.js
@@ -1,19 +1,46 @@
 import { Howl, Howler } from 'howler';
 import highBeepPath from '../assets/sound/high_beep.mp3';
 import lowBeepPath from '../assets/sound/low_beep.mp3';
+import high from '../assets/sound/High.mp3';
+import normal from '../assets/sound/Normal.mp3';
+import low from '../assets/sound/Low.mp3';
 
 export function highBeep(volume) {
-  const highBeep = new Howl({
+  const highBeepSound = new Howl({
     src: [highBeepPath],
-    volume: volume / 100,
+    volume: volume / 100
   });
-  highBeep.play();
+  highBeepSound.play();
 }
 
 export function lowBeep(volume) {
-  const lowBeep = new Howl({
+  const lowBeepSound = new Howl({
     src: [lowBeepPath],
-    volume: volume / 100,
+    volume: volume / 100
   });
-  lowBeep.play();
+  lowBeepSound.play();
+}
+
+export function high(volume){
+  const highSound = new Howl({
+    src: [high],
+    volume: volume / 100
+  });
+  highSound.play();
+}
+
+export function normal(volume){
+  const normalSound = new Howl({
+    src: [normal],
+    volume: volume / 100
+  });
+  normalSound.play();
+}
+
+export function low(volume){
+  const lowSound = new Howl({
+    src: [low],
+    volume: volume / 100
+  });
+  lowSound.play();
 }
