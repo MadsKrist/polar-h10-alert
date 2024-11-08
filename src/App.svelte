6329ae7dea6c795bb0c25b74f9e0ff0e8e961a7f
MadsKrist <163913343+MadsKrist@users.noreply.github.com>
2024-11-08 13:34:18 +0100

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -45,9 +45,9 @@
     heartRate = 0;
     isTooLow = false;
     isTooHigh = false;
-    let highAlerted = false;
-    let normalAlerted = false;
-    let lowAlerted = false;
+    highAlerted = false;
+    normalAlerted = false;
+    lowAlerted = false;
     step = 'run';
 
     try {
