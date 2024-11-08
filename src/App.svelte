135fc5f7d5d71028ff1cd4fdfed2f234009211cf
MadsKrist <163913343+MadsKrist@users.noreply.github.com>
2024-11-08 13:03:21 +0100

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -83,7 +83,7 @@
       lowAlerted = false;
 	    high(volume[0]);
     }
-    else if(heartRate > 80 && normalAlerted == false)
+    else if(heartRate < 90 && normalAlerted == false)
     {
       console.log("alerted normal");
    	  normalAlerted = true;
