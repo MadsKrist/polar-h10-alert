d00eeadc1195f276b29617231a6f42d12f7fe1b6
MadsKrist <163913343+MadsKrist@users.noreply.github.com>
2024-11-08 13:00:09 +0100

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -75,7 +75,7 @@
     const [min, max] = heartRange;
 
 
-    if(heartRate > 110 && highAlerted == false)
+    if(heartRate > 90 && highAlerted == false)
     {
       console.log("alerted high");
 	    highAlerted = true;
@@ -83,7 +83,7 @@
       lowAlerted = false;
 	    high(volume[0]);
     }
-    else if(heartRate > 100 && normalAlerted == false)
+    else if(heartRate > 80 && normalAlerted == false)
     {
       console.log("alerted normal");
    	  normalAlerted = true;
@@ -91,7 +91,7 @@
 	    lowAlerted = false;
 	    normal(volume[0]);
     }
-    else if(heartRate < 90 && lowAlerted == false)
+    else if(heartRate < 70 && lowAlerted == false)
     {
       console.log("alerted low");
     	lowAlerted = true;
